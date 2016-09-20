8734f808ad7fb13beaf71f3ad4fded72467af8a2
George Lacey <georgelaceyy@gmail.com>
2016-09-20 18:35:13 +0100

    Rename log_entry.py -> LogEntry.py

--- LogEntry.py
+++ LogEntry.py
@@ -1,32 +1,16 @@
 from datetime import *
 import re
 
-class log_entry(object):
-    
+
+class logEntry(object):
+
     def __init__(self, name, fingerprint, datetime_string, duration,
-                    file_count):
+                 file_count):
         self.name = name
         self.fingerprint = fingerprint
         self.datetime = self.set_datetime(datetime_string)
         self.duration = duration
         self.file_count = file_count
-    
-    def set_datetime(self, datetime_string):
-        
-        date = re.search(r"....-..-..", datetime_string).group()
-        time = re.search(r"..:..:..", datetime_string).group()
-        
-        year = int(date[0:4])
-        month = int(date[5:7])
-        day = int(date[8:10])
-        
-        hour = int(time[0:2])
-        minute = int(time[3:5])
-        second = int(time[6:8])
-        
-        converted_datetime = datetime(year, month, day, hour, minute, second)
-        
-        return converted_datetime
     
     def print_to_file(self, filename):
         x = open(filename, "w")
@@ -39,3 +23,20 @@ class log_entry(object):
         x.write("file_count: %s\n" % self.file_count)
         
         x.close()
+
+
+def set_datetime(datetime_string):
+    date = re.search(r"....-..-..", datetime_string).group()
+    time = re.search(r"..:..:..", datetime_string).group()
+
+    year = int(date[0:4])
+    month = int(date[5:7])
+    day = int(date[8:10])
+
+    hour = int(time[0:2])
+    minute = int(time[3:5])
+    second = int(time[6:8])
+
+    converted_datetime = datetime(year, month, day, hour, minute, second)
+
+    return converted_datetime
