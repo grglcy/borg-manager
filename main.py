da47c5a3fbf273025ecff2f637c906095d5d407c
George Lacey <georgelaceyy@gmail.com>
2016-09-20 18:36:20 +0100

    Refactor log_entry -> LogEntry

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from sys import stdin
-from log_entry import *
+from LogEntry import *
 
 raw_borg_output = stdin.readlines()
 
-attributes = {  "Archive name: " : "",
-                "Archive fingerprint: " : "",
-                "Time (start): " : "",
-                "Duration: " : "",
-                "Number of files: " : "", }
+attributes = {"Archive name: ": "",
+              "Archive fingerprint: ": "",
+              "Time (start): ": "",
+              "Duration: ": "",
+              "Number of files: ": ""}
                 
 for i in range(0, len(raw_borg_output)):
     
     for key in attributes:
         if raw_borg_output[i].startswith(key):
             attributes[key] = raw_borg_output[i] \
-            .strip(key) \
-            .rstrip()
+                .strip(key) \
+                .rstrip()
 
-borg_output = log_entry(attributes["Archive name: "],
-                        attributes["Archive fingerprint: "],
-                        attributes["Time (start): "],
-                        attributes["Duration: "],
-                        attributes["Number of files: "])
+borg_output = LogEntry(attributes["Archive name: "],
+                       attributes["Archive fingerprint: "],
+                       attributes["Time (start): "],
+                       attributes["Duration: "],
+                       attributes["Number of files: "])
 
 borg_output.print_to_file("borg.txt")
