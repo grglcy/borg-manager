a663450c262bfec5c5eb9c6db6cd9d85ff6a00c1
George Lacey <git@george.ooo>
2021-05-04 03:02:42 +0100

    Create table in constructor

--- src/database.py
+++ src/database.py
@@ -6,6 +6,7 @@ class Database(object):
     def __init__(self, path):
         self.conn = sqlite3.connect(path)
         self.table_name = "log"
+        self.create_log_table()
     
     def __del__(self):
         self.conn.close()
@@ -25,7 +26,7 @@ class Database(object):
         self.commit()
 
     def insert(self, log_entry: LogEntry):
-        query = f"INSERT INTO {self.table_name} (name, fingerprint, start, end, duration, filecount) VALUES(?,?,?,?,?)"
+        query = f"INSERT INTO {self.table_name} (name, fingerprint, start, end, filecount) VALUES(?,?,?,?,?)"
         self.conn.execute(query, (log_entry.name, log_entry.fingerprint, log_entry.start_time, log_entry.end_time,
                                   log_entry.file_count))
         self.commit()
